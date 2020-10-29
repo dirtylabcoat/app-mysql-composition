e613038d7a23d8d76bab2fe060cfb6b7ddb14881
Fighter Hayabusa <fighterhayabusa@dirtylabcoat.org>
2020-10-29 11:44:04 +0100

    Tiny JPA fixes

--- app/src/main/java/com/example/app/db/Tbl.java
+++ app/src/main/java/com/example/app/db/Tbl.java
@@ -1,13 +1,18 @@
 package com.example.app.db;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tbl")
 public class Tbl {
 
     @Id
+    @Column(name = "id")
     private Long id;
+    @Column(name = "msg")
     private String msg;
 
     public Long getId() {
